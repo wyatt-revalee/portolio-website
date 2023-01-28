c281190be0c280541a8b513f7a084e54ba02c868
Wyatt Revalee <wyatt.revalee@gmail.com>
2023-01-28 14:17:39 -0500

    JS WIP. Added piece pngs

--- chessGame/chess.js
+++ chessGame/chess.js
@@ -1,21 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
+//Insert images for the pieces
+function insertImage() {
+    document.querySelectorAll('.box').forEach(image => {
+        
+        if(image.innerText.length !== 0) {
+            if (image.innerText == 'Wpawn' || image.innerText == 'Bpawn') {
+                
+                image.innerHTML = '${image.innerText} <img class='allimg allpawn' src="images/${image.innerText}.png" alt="">
+                image.style.cursor = 'pointer'
+            }
+        
+        else {
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="author" content="Wyatt Revalee">
-        <link rel="stylesheet" href="Chess.css">
-
-        <title>Chess</title>
-    </head>
-
-    <body>
-
-        <h1>Chess</h1>
-        <h2 id="tog">White's Turn</h2>
-
-    </body>
-
-</html>
+            image.innerHTML = '${image.innerText} <img class='allimg' src="images/${image.innerText}.png" alt="">
+            image.style.cursor = 'pointer'
+        }
+        }
+    })
+}
