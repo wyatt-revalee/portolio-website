17fc0ec81a1aa6d219126d60362c0b707aedf45b
Wyatt Revalee <wyatt.revalee@gmail.com>
2023-01-28 13:45:52 -0500

    Started work on chess game

---
+++ chessGame/chess.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="Wyatt Revalee">
+        <link rel="stylesheet" href="Chess.css">
+
+        <title>Chess</title>
+    </head>
+
+    <body>
+
+        <h1>Chess</h1>
+        <h2 id="tog">White's Turn</h2>
+
+    </body>
+
+</html>
